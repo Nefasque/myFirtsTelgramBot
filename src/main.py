052c9ef258c083ed63395324bdb95224f28c0850
Nefasque <Pedro0fici4lDev@gmail.com>
2024-02-07 01:19:49 -0400

    handler test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import telebot
 from dotenv import load_dotenv
 import os
+import logging as logger
 
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -24,13 +25,155 @@ def send_welcome(message):
 
 
 # tipe filter in message_handler
-# handle all sent document and audio files
 
-
+####
 # content_types=["text", "sticker", "pinned_message", "photo", "audio"]
-@bot.message_handler(content_types=["document", "audio"])
+####
+
+
+# handle all sent audio files
+@bot.message_handler(content_types=["audio"])
 def handle_docs_audio(message):
-    bot.reply_to(message, "Sorry, I can't read documents or audio files.")
+    bot.reply_to(message, "this is audio.")
 
 
-bot.infinity_polling()
+# handle all sent photo
+@bot.message_handler(content_types=["photo"])
+def handle_docs_photo(message):
+    bot.reply_to(message, "this is photo.")
+
+
+# handle all sent video
+@bot.message_handler(content_types=["video"])
+def handle_docs_video(message):
+    bot.reply_to(message, "this is video.")
+
+
+# handle all sent voice
+@bot.message_handler(content_types=["voice"])
+def handle_docs_voice(message):
+    bot.reply_to(message, "this is voice.")
+
+
+# handle all other messages for wich the lambda
+# returns true.
+# (true whe there is a document and is if this is type text/plain)
+@bot.message_handler(func=lambda
+                     message: message.document.mime_type == 'text/plain',
+                     content_types=['document'])
+def handle_text_doc(message):
+    bot.reply_to(message, "this document is type 'text/plain'")
+
+
+# handle all sent document
+@bot.message_handler(content_types=["document"])
+def handle_docs_doc(message):
+    bot.reply_to(message, "this is document.")
+
+
+# handle all sent sticker
+@bot.message_handler(content_types=["sticker"])
+def handle_docs_sticker(message):
+    bot.reply_to(message, "this is sticker.")
+
+
+# handles all text messages that match the regular
+# expression with regexp
+@bot.message_handler(regexp="^Book")
+def handle_text(message):
+    bot.reply_to(message, "I love books.")
+
+
+# handle all text
+# @bot.message_handler(content_types=["text"])
+# def handle_text(message):
+#     bot.reply_to(message, "this is message.text: " + message.text)
+
+
+
+
+# wich could also be defined as
+def is_textPlain(message):
+    return message.document.mime_type == 'text/plain'
+
+
+@bot.message_handler(func=is_textPlain, content_types=['document'])
+def handle_text_doc(message):
+    bot.reply_to(message, "this document is type 'text/plain' - seccon way")
+
+
+# handlers can be stacked to create a function wich be
+# called if either message_handler is eligible
+# this handler will be called if the message start with '/hellor'
+# or is some emoji
+@bot.message_handler(commands=['hello'])
+@bot.message_handler(func=lambda message: '👋' in message.text)
+def send_hello(message):
+    bot.reply_to(message, "Hello!")
+
+
+#####
+# edited messages handlers
+# passes a Message type object to your function
+@bot.edited_message_handler(func=lambda message: True)
+def echo_edited(message):
+    bot.reply_to(message, "you edited message")
+
+
+# channel post handler
+# passes a Message type object to your function
+@bot.channel_post_handler(func=lambda message: True)
+def echo_channel(message):
+    bot.reply_to(message, "you send channel message")
+
+
+# edited channel post handler
+# passes a Message type object to your function
+@bot.edited_channel_post_handler(func=lambda message: True)
+def echo_edited_channel(message):
+    bot.reply_to(message, "you edited channel message")
+
+
+######
+# callback query handler
+# passes a CallbackQuery type object to your function
+@bot.callback_query_handler(func=lambda call: True)
+def test_callback(call):
+    logger.info(call)
+
+
+# shipping query handler
+# passes a ShippingQuery type object to your function
+@bot.shipping_query_handler(func=lambda query: True)
+def test_shipping(query):
+    logger.info(query)
+
+
+# poll handler -> encuensta
+# passes a Poll type object to your function
+# @bot.poll_handler(func=lambda message: True)
+@bot.message_handler(content_types=['poll'])
+def create_poll(message):
+    bot.send_message(message.chat.id, "you send poll")
+
+
+# poll asnwer handler -> encuensta
+# passes a PollAnswer type object to your function
+@bot.poll_answer_handler()
+def poll_answer(message):
+    bot.reply_to(message, "you send poll answer")
+
+
+####
+# my chat menber handler
+# passes a ChatMemberUpdated type object to your function
+@bot.my_chat_member_handler()
+def test_member(message):
+    chat_id = message.chat.id
+    new_status = message.new_chat_member.status
+    if new_status == 'member':
+        bot.send_message(chat_id, "hola soy el nuevo miembro :D")
+
+
+# infinite loop
+bot.infinity_polling(none_stop=True)
